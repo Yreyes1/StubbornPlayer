ce1d58e386081b43be8cbb6f9e39f49fb5b7b365
yreyes1 <yreyes1@example.com>
2018-03-01 11:39:06 -0800

    how many times do you wnat to play? and output of wins

--- main.cpp
+++ main.cpp
@@ -6,6 +6,31 @@ using namespace std;
 int main()
 {
   SecretDoor game;
+  int NumberOfGames;
+  int Wins = 0;
+
+  cout<<"How many times do you want to play?\n";
+  cin>>NumberOfGames;
+ 
+ for(int i = 0;i < NumberOfGames; i++)
+ {
+    game.newGame();
+    game.guessDoorC();
+    //cout<<"You have chosen door C\n";
+    game.guessDoorC();
+    //cout<<"You have chosen door C\n";
+   if(game.isWinner())
+   {    // cout<<"You win!\n";
+        Wins++;
+   }
+    else
+    {
+       // cout<<"You lose\n";
+    }
     
+ }
+        
+cout<<"You won " <<Wins<< " out of " <<NumberOfGames<< " games. \n";
+
   return 0;
 }
